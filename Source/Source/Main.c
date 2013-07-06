5429f0754f1823cde50398c300d94a73831e1188
Rico <teh_programerer@interactivelaboratories.com>
2013-07-06 02:13:07 +0000

    |SOURCE| [Main] * Using the GX interface to render with * A basic loop which clears the colour on V-Sync is implemented

--- Source/Source/Main.c
+++ Source/Source/Main.c
@@ -6,56 +6,82 @@
 #include <gccore.h>
 #include <GitVersion.h>
 
-static void *xfb = NULL;
-static GXRModeObj *rmode = NULL;
+#define DEFAULT_FIFO_SIZE	( 256*1024 )
+static void *g_sppFrameBuffer[ 2 ] = { NULL, NULL };
+static GXRModeObj *g_pMode = NULL;
 
-void *Initialise();
+int main( int argc, char **argv )
+{
+	f32		YScale;
+	u32		FBHeight;
+	u32		FBIndex = 0;
+	GXColor	ClearColour = { 0x20, 0x00, 0x20, 0xFF };
 
-int main(int argc, char **argv) {
+	VIDEO_Init( );
 
-	xfb = Initialise();
+	g_pMode = VIDEO_GetPreferredMode( NULL );
+	PAD_Init( );
 
-	printf("\nTwenty|One\n");
-	printf( "\nRevision: %s\n", GIT_COMMITHASH );
+	g_sppFrameBuffer[ 0 ] = MEM_K0_TO_K1( SYS_AllocateFramebuffer( g_pMode ) );
+	g_sppFrameBuffer[ 1 ] = MEM_K0_TO_K1( SYS_AllocateFramebuffer( g_pMode ) );
 
-	while(1) {
+	VIDEO_Configure( g_pMode );
+	VIDEO_SetNextFramebuffer( g_sppFrameBuffer[ FBIndex ] );
+	VIDEO_SetBlack( FALSE );
+	VIDEO_Flush( );
+	VIDEO_WaitVSync( );
 
-		VIDEO_WaitVSync();
-		PAD_ScanPads();
+	if( g_pMode->viTVMode & VI_NON_INTERLACE )
+	{
+		VIDEO_WaitVSync( );
+	}
 
-		int buttonsDown = PAD_ButtonsDown(0);
+	void *pFIFO = NULL;
+	pFIFO = memalign( 32, DEFAULT_FIFO_SIZE );
+	memset( pFIFO, 0, sizeof( DEFAULT_FIFO_SIZE ) );
+
+	GX_Init( pFIFO, DEFAULT_FIFO_SIZE );
+
+	GX_SetCopyClear( ClearColour, 0x00FFFFFF );
+
+	GX_SetViewport( 0, 0, g_pMode->fbWidth, g_pMode->efbHeight, 0, 1 );
+	YScale = GX_GetYScaleFactor( g_pMode->efbHeight, g_pMode->xfbHeight );
+	FBHeight = GX_SetDispCopyYScale( YScale );
+	GX_SetScissor( 0, 0, g_pMode->fbWidth, g_pMode->efbHeight );
+	GX_SetDispCopySrc( 0, 0, g_pMode->fbWidth, g_pMode->efbHeight );
+	GX_SetDispCopyDst( g_pMode->fbWidth, FBHeight );
+	GX_SetCopyFilter( g_pMode->aa, g_pMode->sample_pattern, GX_TRUE,
+		g_pMode->vfilter );
+	GX_SetFieldMode( g_pMode->field_rendering,
+		( ( g_pMode->viHeight == 2*g_pMode->xfbHeight ) ?
+			GX_ENABLE : GX_DISABLE ) );
+	GX_SetCullMode( GX_CULL_NONE );
+	GX_CopyDisp( g_sppFrameBuffer[ FBIndex ], GX_TRUE );
+	GX_SetDispCopyGamma( GX_GM_1_0 );
+
+	while( 1 )
+	{
+		PAD_ScanPads( );
+
+		int ButtonsDown = PAD_ButtonsDown( 0 );
 		
-		if( buttonsDown & PAD_BUTTON_A ) {
-			printf("Button A pressed.\n");
+		if( ButtonsDown & PAD_BUTTON_START )
+		{
+			exit( 0 );
 		}
 
-		if (buttonsDown & PAD_BUTTON_START) {
-			exit(0);
-		}
+		GX_SetViewport( 0, 0, g_pMode->fbWidth, g_pMode->efbHeight, 0, 1 );
+		GX_DrawDone( );
+		FBIndex ^= 1;
+		GX_SetZMode( GX_TRUE, GX_LEQUAL, GX_TRUE );
+		GX_SetColorUpdate( GX_TRUE );
+		GX_CopyDisp( g_sppFrameBuffer[ FBIndex ], GX_TRUE );
+		VIDEO_SetNextFramebuffer( g_sppFrameBuffer[ FBIndex ] );
+		VIDEO_Flush( );
+		VIDEO_WaitVSync( );
 	}
 
 	return 0;
 }
 
-void * Initialise() {
 
-	void *framebuffer;
-
-	VIDEO_Init();
-	PAD_Init();
-	
-	rmode = VIDEO_GetPreferredMode(NULL);
-
-	framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(framebuffer,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-	
-	VIDEO_Configure(rmode);
-	VIDEO_SetNextFramebuffer(framebuffer);
-	VIDEO_SetBlack(FALSE);
-	VIDEO_Flush();
-	VIDEO_WaitVSync();
-	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-
-	return framebuffer;
-
-}
